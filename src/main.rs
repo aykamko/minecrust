fefd9e268e158a74fd0d069cb27bff6251681944
Aleks <aykamko@gmail.com>
2022-06-19 15:01:12 -0700

    Initial commit -- trying to initialize a window with wgpu

---
+++ src/main.rs
@@ -0,0 +1,94 @@
+use winit::{
+    // event::{Event, WindowEvent},
+    event_loop::{/* ControlFlow, */ EventLoop},
+    // window::WindowBuilder,
+};
+use wgpu;
+use futures::executor::block_on;
+
+fn main() {
+    let s = block_on(setup());
+    start(setup);
+}
+
+// #[cfg(not(target_arch = "wasm32"))]
+// pub fn run<E: Example>(title: &str) {
+//     let setup = pollster::block_on(setup::<E>(title));
+//     start::<E>(setup);
+// }
+struct Setup {
+    window: winit::window::Window,
+    event_loop: EventLoop<()>,
+    instance: wgpu::Instance,
+    size: winit::dpi::PhysicalSize<u32>,
+    surface: wgpu::Surface,
+    adapter: wgpu::Adapter,
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+    // #[cfg(target_arch = "wasm32")]
+    // offscreen_canvas_setup: Option<OffscreenCanvasSetup>,
+}
+
+async fn setup() -> Setup {
+    let event_loop = EventLoop::new();
+    let mut builder = winit::window::WindowBuilder::new();
+    builder = builder.with_title("Minecrust");
+    let window = builder.build(&event_loop).unwrap();
+
+    let backend = wgpu::Backends::PRIMARY;
+    let instance = wgpu::Instance::new(backend);
+
+    let size = window.inner_size();
+    let surface = unsafe {
+       let surface = instance.create_surface(&window);
+       surface
+    };
+
+    let adapter =
+        wgpu::util::initialize_adapter_from_env_or_default(&instance, backend, Some(&surface))
+            .await
+            .expect("No suitable GPU adapters found on the system!");
+
+    let adapter_info = adapter.get_info();
+    println!("Using {} ({:?})", adapter_info.name, adapter_info.backend);
+
+    let trace_dir = std::env::var("WGPU_TRACE");
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: None,
+                features: wgpu::Features::default(),
+                limits: wgpu::Limits::default(),
+            },
+         trace_dir.ok().as_ref().map(std::path::Path::new),
+        )
+        .await
+        .expect("Unable to find a suitable GPU adapter!");
+
+    Setup {
+        window,
+        event_loop,
+        instance,
+        size,
+        surface,
+        adapter,
+        device,
+        queue,
+    }
+
+    // event_loop.run(move |event, _, control_flow| {
+    //     *control_flow = ControlFlow::Wait;
+
+    //     match event {
+    //         Event::WindowEvent {
+    //             event: WindowEvent::CloseRequested,
+    //             window_id,
+    //         } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+    //         _ => (),
+    //     }
+    // });
+}
+
+fn start(setup: Setup) {
+
+}
