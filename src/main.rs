fd5754c6a53212fa2ae1a6f8b11ddb6220ec8cc5
Aleks <aykamko@gmail.com>
2022-06-19 15:37:14 -0700

    Some vertex data copied from cube example

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
+use futures::executor::block_on;
+use wgpu::util::DeviceExt;
 use winit::{
     event::{Event, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
 };
-use wgpu;
-use futures::executor::block_on;
+// use std::{f32::consts, mem, pin::Pin, task};
+use bytemuck::{Pod, Zeroable};
 
 fn main() {
     let s = block_on(setup());
     start(s);
 }
 
-// #[cfg(not(target_arch = "wasm32"))]
-// pub fn run<E: Example>(title: &str) {
-//     let setup = pollster::block_on(setup::<E>(title));
-//     start::<E>(setup);
-// }
 struct Setup {
     window: winit::window::Window,
     event_loop: EventLoop<()>,
@@ -39,8 +36,8 @@ async fn setup() -> Setup {
 
     let size = window.inner_size();
     let surface = unsafe {
-       let surface = instance.create_surface(&window);
-       surface
+        let surface = instance.create_surface(&window);
+        surface
     };
 
     let adapter =
@@ -59,7 +56,7 @@ async fn setup() -> Setup {
                 features: wgpu::Features::default(),
                 limits: wgpu::Limits::default(),
             },
-         trace_dir.ok().as_ref().map(std::path::Path::new),
+            trace_dir.ok().as_ref().map(std::path::Path::new),
         )
         .await
         .expect("Unable to find a suitable GPU adapter!");
@@ -76,7 +73,8 @@ async fn setup() -> Setup {
     }
 }
 
-fn start(Setup {
+fn start(
+    Setup {
         window,
         event_loop,
         instance,
@@ -85,10 +83,11 @@ fn start(Setup {
         adapter,
         device,
         queue,
-}: Setup) {
+    }: Setup,
+) {
     let format = surface
-            .get_preferred_format(&adapter)
-            .expect("No suitable format found on the system!");
+        .get_preferred_format(&adapter)
+        .expect("No suitable format found on the system!");
     let mut config = wgpu::SurfaceConfiguration {
         usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
         format: format,
@@ -97,6 +96,8 @@ fn start(Setup {
         present_mode: wgpu::PresentMode::Mailbox,
     };
     surface.configure(&device, &config);
+
+    draw_scene(&config, &adapter, &device, &queue);
 
     event_loop.run(move |event, _, control_flow| {
         *control_flow = ControlFlow::Poll;
@@ -109,4 +110,78 @@ fn start(Setup {
             _ => (),
         }
     });
+}
+
+fn draw_scene(
+    config: &wgpu::SurfaceConfiguration,
+    _adapter: &wgpu::Adapter,
+    device: &wgpu::Device,
+    queue: &wgpu::Queue,
+) {
+    let (vertex_data, index_data) = create_vertices();
+    let vertex_buf = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("Vertex Buffer"),
+        contents: bytemuck::cast_slice(&vertex_data),
+        usage: wgpu::BufferUsages::VERTEX,
+    });
+}
+
+#[derive(Clone, Copy, Pod, Zeroable)]
+#[repr(C)]
+struct Vertex {
+    _pos: [f32; 4],
+    _tex_coord: [f32; 2],
+}
+
+fn vertex(pos: [i8; 3], tc: [i8; 2]) -> Vertex {
+    Vertex {
+        _pos: [pos[0] as f32, pos[1] as f32, pos[2] as f32, 1.0],
+        _tex_coord: [tc[0] as f32, tc[1] as f32],
+    }
+}
+
+fn create_vertices() -> (Vec<Vertex>, Vec<u16>) {
+    let vertex_data = [
+        // top (0, 0, 1)
+        vertex([-1, -1, 1], [0, 0]),
+        vertex([1, -1, 1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([-1, 1, 1], [0, 1]),
+        // bottom (0, 0, -1)
+        vertex([-1, 1, -1], [1, 0]),
+        vertex([1, 1, -1], [0, 0]),
+        vertex([1, -1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // right (1, 0, 0)
+        vertex([1, -1, -1], [0, 0]),
+        vertex([1, 1, -1], [1, 0]),
+        vertex([1, 1, 1], [1, 1]),
+        vertex([1, -1, 1], [0, 1]),
+        // left (-1, 0, 0)
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, 1, 1], [0, 0]),
+        vertex([-1, 1, -1], [0, 1]),
+        vertex([-1, -1, -1], [1, 1]),
+        // front (0, 1, 0)
+        vertex([1, 1, -1], [1, 0]),
+        vertex([-1, 1, -1], [0, 0]),
+        vertex([-1, 1, 1], [0, 1]),
+        vertex([1, 1, 1], [1, 1]),
+        // back (0, -1, 0)
+        vertex([1, -1, 1], [0, 0]),
+        vertex([-1, -1, 1], [1, 0]),
+        vertex([-1, -1, -1], [1, 1]),
+        vertex([1, -1, -1], [0, 1]),
+    ];
+
+    let index_data: &[u16] = &[
+        0, 1, 2, 2, 3, 0, // top
+        4, 5, 6, 6, 7, 4, // bottom
+        8, 9, 10, 10, 11, 8, // right
+        12, 13, 14, 14, 15, 12, // left
+        16, 17, 18, 18, 19, 16, // front
+        20, 21, 22, 22, 23, 20, // back
+    ];
+
+    (vertex_data.to_vec(), index_data.to_vec())
 }
