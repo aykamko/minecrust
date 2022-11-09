41c494eacf85dba19c04072c049c502f9de8a85a
Aleks <aykamko@gmail.com>
2022-11-09 08:47:47 -0800

    Revert "attempting to fix shadow flickering, no luck"
    This reverts commit 410589f7e922c8f81a4ddfc7ae7e0c5cd1efedde.

--- src/light.rs
+++ src/light.rs
@@ -1,8 +1,7 @@
-use std::f32::consts::PI;
-
 use crate::camera::Camera;
 use crate::vertex::{CuboidCoords, QuadListRenderData, Vertex};
 use glam::{Mat4, Vec3};
+use wgpu::Origin3d;
 
 pub struct LightUniform {
     pub position: Vec3,
@@ -14,11 +13,6 @@ pub struct LightUniform {
     pub sunlight_ortho_proj_coords: CuboidCoords,
     pub sunlight_ortho_proj: glam::Mat4,
     pub shadow_map_pixel_size: [u32; 2],
-    pub texel_size: [f32; 2],
-    pub sun_planar_rotation: glam::Mat3,
-    pub sun_planar_inverse_rotation: glam::Mat3,
-    pub sun_xz_grid_cell_size: [f32; 2],
-    pub sun_xz_grid_pos_remainder: [f32; 2],
 }
 
 #[repr(C)]
@@ -48,37 +42,6 @@ impl LightUniform {
             sunlight_ortho_proj_coords.far,
         );
 
-        let texel_size: [f32; 2] = [
-            (sunlight_ortho_proj_coords.right - sunlight_ortho_proj_coords.left)
-                / shadow_map_pixel_size[0] as f32,
-            (sunlight_ortho_proj_coords.top - sunlight_ortho_proj_coords.bottom)
-                / shadow_map_pixel_size[1] as f32,
-        ];
-
-        let sun_y_angle_rad = (sun_position.y / sun_position.x).atan();
-        let behind_sun_y_angle_rad = ((PI / 2.0) - sun_y_angle_rad).cos();
-
-        let sun_z_angle_rad = (sun_position.z / sun_position.x).atan();
-        let sun_xz_grid_cell_size = [
-            (behind_sun_y_angle_rad.cos() * texel_size[1]) * 2.0,
-            texel_size[0],
-        ];
-
-        let sun_planar_rotation = glam::Mat3::from_rotation_y(-sun_z_angle_rad);
-        let sun_planar_inverse_rotation = glam::Mat3::from_rotation_y(sun_z_angle_rad);
-
-        let sun_pos_planar_normalized = sun_planar_rotation * sun_position;
-
-        let sun_xz_grid_pos_remainder = [
-            //sun_pos_planar_normalized.x
-            //    - (texel_size[0] * (sun_pos_planar_normalized.x / texel_size[0]).floor()),
-            //sun_pos_planar_normalized.z
-            //    - (texel_size[1] * (sun_pos_planar_normalized.z / texel_size[1]).floor()),
-                0.0,0.0
-        ];
-
-        println!("Remainder is {:?}", sun_xz_grid_pos_remainder);
-
         Self {
             position,
             color,
@@ -89,11 +52,6 @@ impl LightUniform {
             sunlight_ortho_proj_coords,
             sunlight_ortho_proj,
             shadow_map_pixel_size,
-            texel_size,
-            sun_planar_rotation,
-            sun_planar_inverse_rotation,
-            sun_xz_grid_cell_size,
-            sun_xz_grid_pos_remainder,
         }
     }
 
@@ -119,31 +77,15 @@ impl LightUniform {
     }
 
     pub fn update_light_space_proj(&mut self, camera: &Camera) {
-        let sun_adjust = camera.initial_eye - camera.eye;
+        // let sun_adjust = Vec3::new(
+        //     f32::trunc(camera.eye.x) - camera.eye.x,
+        //     camera.initial_eye.y - camera.eye.y,
+        //     f32::trunc(camera.eye.z) - camera.eye.z,
+        // );
+        let sun_adjust = Vec3::new(0.0, camera.initial_eye.y - camera.eye.y, 0.0);
 
-        let pos_adjusted_before_texel_normalization =
-            self.sun_position + Vec3::new(sun_adjust.x, sun_adjust.y, sun_adjust.z);
-        let pos_rotated = self.sun_planar_rotation * pos_adjusted_before_texel_normalization;
-        let pos_rotated_texel_adjusted = Vec3::new(
-            (self.sun_xz_grid_cell_size[0] * (pos_rotated.x / self.sun_xz_grid_cell_size[0]).floor()) + self.sun_xz_grid_pos_remainder[0],
-            pos_rotated.y,
-            (self.sun_xz_grid_cell_size[1] * (pos_rotated.z / self.sun_xz_grid_cell_size[1]).floor()) + self.sun_xz_grid_pos_remainder[1],
-        );
-        let texel_adjusted_sun_pos = self.sun_planar_inverse_rotation * pos_rotated_texel_adjusted;
-
-        let target_adjusted_before_texel_normalization =
-            self.sun_target + Vec3::new(sun_adjust.x, sun_adjust.y, sun_adjust.z);
-        let target_rotated = self.sun_planar_rotation * target_adjusted_before_texel_normalization;
-        let target_rotated_texel_adjusted = Vec3::new(
-            (self.sun_xz_grid_cell_size[0] * (target_rotated.x / self.sun_xz_grid_cell_size[0]).floor()) + self.sun_xz_grid_pos_remainder[0],
-            target_rotated.y,
-            (self.sun_xz_grid_cell_size[1] * (target_rotated.z / self.sun_xz_grid_cell_size[1]).floor()) + self.sun_xz_grid_pos_remainder[1],
-        );
-        let texel_adjusted_sun_target = self.sun_planar_inverse_rotation * target_rotated_texel_adjusted;
-
-        self.sun_position_camera_adjusted = texel_adjusted_sun_pos;
-        self.sun_target_camera_adjusted = texel_adjusted_sun_target;
-        println!("Sun position: {:?}, sun target: {:?}", self.sun_position_camera_adjusted, self.sun_target_camera_adjusted);
+        self.sun_position_camera_adjusted = self.sun_position + sun_adjust;
+        self.sun_target_camera_adjusted = self.sun_target + sun_adjust;
     }
 
     pub fn vertex_data_for_sunlight(&self) -> QuadListRenderData {
