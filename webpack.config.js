f5c879412a408af22927591a2a22980bdf5b191c
Aleks <aykamko@gmail.com>
2022-11-18 16:51:03 -0800

    switch to typescript

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,22 @@ const dist = path.resolve(__dirname, "dist");
 module.exports = {
   mode: "production",
   entry: {
-    index: "./js/index.js"
+    index: "./ts/index.ts"
+  },
+  experiments: {
+    asyncWebAssembly: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     path: dist,
