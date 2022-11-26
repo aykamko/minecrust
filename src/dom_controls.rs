a0c860035ad1aa2f281b904990314431514226aa
Aleks <aykamko@gmail.com>
2022-11-26 14:08:32 -0800

    move DOM controls into a separate file

---
+++ src/dom_controls.rs
@@ -0,0 +1,80 @@
+use winit::event_loop::{EventLoop, EventLoopProxy};
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[derive(Debug)]
+pub enum DomControlsUserEvent {
+    AButtonPressed,
+    AButtonReleased,
+    BButtonPressed,
+    BButtonReleased,
+    PitchYawJoystickMoved { vector: (f64, f64) },
+    PitchYawJoystickReleased,
+    TranslationJoystickMoved { vector: (f64, f64) },
+    TranslationJoystickReleased,
+    WindowResized { size: winit::dpi::LogicalSize<u32> },
+}
+
+struct EventLoopGlobalState {
+    event_loop_proxy: Option<EventLoopProxy<DomControlsUserEvent>>,
+}
+static mut event_loop_global_state: EventLoopGlobalState = EventLoopGlobalState {
+    event_loop_proxy: None,
+};
+
+pub unsafe fn set_global_event_loop_proxy(event_loop: &EventLoop<DomControlsUserEvent>) {
+    event_loop_global_state.event_loop_proxy = Some(event_loop.create_proxy());
+}
+
+fn send_dom_controls_user_event(event: DomControlsUserEvent) {
+    let event_loop_proxy = unsafe {
+        match event_loop_global_state.event_loop_proxy {
+            None => return,
+            _ => event_loop_global_state.event_loop_proxy.as_ref().unwrap(),
+        }
+    };
+
+    event_loop_proxy
+        .send_event(event)
+        .expect("Failed to queue DOM button event");
+}
+
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn a_button_pressed() {
+    send_dom_controls_user_event(DomControlsUserEvent::AButtonPressed);
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn a_button_released() {
+    send_dom_controls_user_event(DomControlsUserEvent::AButtonReleased);
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn b_button_pressed() {
+    send_dom_controls_user_event(DomControlsUserEvent::BButtonPressed);
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn b_button_released() {
+    send_dom_controls_user_event(DomControlsUserEvent::BButtonReleased);
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn pitch_yaw_joystick_moved(x: f64, y: f64) {
+    send_dom_controls_user_event(DomControlsUserEvent::PitchYawJoystickMoved { vector: (x, y) });
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn pitch_yaw_joystick_released() {
+    send_dom_controls_user_event(DomControlsUserEvent::PitchYawJoystickReleased);
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn translation_joystick_moved(x: f64, y: f64) {
+    send_dom_controls_user_event(DomControlsUserEvent::TranslationJoystickMoved { vector: (x, y) });
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn translation_joystick_released() {
+    send_dom_controls_user_event(DomControlsUserEvent::TranslationJoystickReleased);
+}
+#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
+pub fn web_window_resized(width: u32, height: u32) {
+    send_dom_controls_user_event(DomControlsUserEvent::WindowResized {
+        size: winit::dpi::LogicalSize { width, height },
+    });
+}
--- src/lib.rs
+++ src/lib.rs
@@ -14,6 +14,7 @@ pub mod texture;
 pub mod vec_extra;
 pub mod vertex;
 pub mod world;
+pub mod dom_controls;
 
 use cgmath::{prelude::*, Point3};
 use futures::executor::block_on;
@@ -25,9 +26,10 @@ use vertex::QuadListRenderData;
 use wgpu::util::DeviceExt;
 use winit::{
     event::{DeviceEvent, ElementState, Event, MouseButton, VirtualKeyCode, WindowEvent},
-    event_loop::{ControlFlow, EventLoop, EventLoopProxy},
+    event_loop::{ControlFlow, EventLoop, EventLoopProxy, EventLoopBuilder},
 };
 use world::{CHUNK_XZ_SIZE, CHUNK_Y_SIZE, VISIBLE_CHUNK_WIDTH};
+use dom_controls::{DomControlsUserEvent};
 
 use crate::world::{Chunk, ChunkDataType, MAX_CHUNK_WORLD_WIDTH};
 
@@ -51,19 +53,6 @@ pub fn run(width: usize, height: usize) {
     start(s);
 }
 
-#[derive(Debug)]
-pub enum DomControlsUserEvent {
-    AButtonPressed,
-    AButtonReleased,
-    BButtonPressed,
-    BButtonReleased,
-    PitchYawJoystickMoved { vector: (f64, f64) },
-    PitchYawJoystickReleased,
-    TranslationJoystickMoved { vector: (f64, f64) },
-    TranslationJoystickReleased,
-    WindowResized { size: winit::dpi::LogicalSize<u32> },
-}
-
 struct Setup {
     window: winit::window::Window,
     event_loop: EventLoop<DomControlsUserEvent>,
@@ -74,8 +63,6 @@ struct Setup {
     adapter: wgpu::Adapter,
     device: wgpu::Device,
     queue: wgpu::Queue,
-    // #[cfg(target_arch = "wasm32")]
-    // offscreen_canvas_setup: Option<OffscreenCanvasSetup>,
 }
 
 struct AnnotatedInstanceBuffer {
@@ -84,6 +71,7 @@ struct AnnotatedInstanceBuffer {
     data_type: world::ChunkDataType,
 }
 struct ChunkRenderDescriptor {
+    #[allow(dead_code)]
     world_chunk_position: [usize; 2],
     annotated_instance_buffers: Vec<AnnotatedInstanceBuffer>,
 }
@@ -111,67 +99,8 @@ struct Scene {
     pipeline_wire_no_instancing: Option<wgpu::RenderPipeline>,
 }
 
-struct EventLoopGlobalState {
-    event_loop_proxy: Option<EventLoopProxy<DomControlsUserEvent>>,
-}
-static mut event_loop_global_state: EventLoopGlobalState = EventLoopGlobalState {
-    event_loop_proxy: None,
-};
-
-fn send_dom_controls_user_event(event: DomControlsUserEvent) {
-    let event_loop_proxy = unsafe {
-        match event_loop_global_state.event_loop_proxy {
-            None => return,
-            _ => event_loop_global_state.event_loop_proxy.as_ref().unwrap(),
-        }
-    };
-
-    event_loop_proxy
-        .send_event(event)
-        .expect("Failed to queue DOM button event");
-}
-
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn a_button_pressed() {
-    send_dom_controls_user_event(DomControlsUserEvent::AButtonPressed);
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn a_button_released() {
-    send_dom_controls_user_event(DomControlsUserEvent::AButtonReleased);
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn b_button_pressed() {
-    send_dom_controls_user_event(DomControlsUserEvent::BButtonPressed);
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn b_button_released() {
-    send_dom_controls_user_event(DomControlsUserEvent::BButtonReleased);
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn pitch_yaw_joystick_moved(x: f64, y: f64) {
-    send_dom_controls_user_event(DomControlsUserEvent::PitchYawJoystickMoved { vector: (x, y) });
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn pitch_yaw_joystick_released() {
-    send_dom_controls_user_event(DomControlsUserEvent::PitchYawJoystickReleased);
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn translation_joystick_moved(x: f64, y: f64) {
-    send_dom_controls_user_event(DomControlsUserEvent::TranslationJoystickMoved { vector: (x, y) });
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn translation_joystick_released() {
-    send_dom_controls_user_event(DomControlsUserEvent::TranslationJoystickReleased);
-}
-#[cfg_attr(target_arch = "wasm32", wasm_bindgen)]
-pub fn web_window_resized(width: u32, height: u32) {
-    send_dom_controls_user_event(DomControlsUserEvent::WindowResized {
-        size: winit::dpi::LogicalSize { width, height },
-    });
-}
-
 async fn setup(width: usize, height: usize) -> Setup {
-    let event_loop = EventLoop::<DomControlsUserEvent>::with_user_event();
+    let event_loop = EventLoopBuilder::<DomControlsUserEvent>::with_user_event().build();
     let mut builder = winit::window::WindowBuilder::new();
     builder = builder.with_title("Minecrust");
     builder = builder.with_inner_size(winit::dpi::LogicalSize {
@@ -190,7 +119,7 @@ async fn setup(width: usize, height: usize) -> Setup {
     let instance = wgpu::Instance::new(backend);
 
     unsafe {
-        event_loop_global_state.event_loop_proxy = Some(event_loop.create_proxy());
+    crate::dom_controls::set_global_event_loop_proxy(&event_loop);
     }
 
     #[cfg(target_arch = "wasm32")]
