5f90ee281c549b2918467c786541000ae30b0743
Aleks <aykamko@gmail.com>
2023-12-26 16:08:54 -0500

    toggleable flying mode

--- src/lib.rs
+++ src/lib.rs
@@ -1347,13 +1347,13 @@ pub fn run(width: usize, height: usize) {
                 }
                 WindowEvent::KeyboardInput { input, .. } => {
                     match (input.virtual_keycode, input.state) {
-                        // (Some(VirtualKeyCode::W), ElementState::Pressed) => {
-                        //     if curr_modifier_state.logo() {
-                        //         *control_flow = ControlFlow::Exit;
-                        //         return;
-                        //     }
-                        //     game.state.camera_controller.process_window_event(&event);
-                        // }
+                        // Toggle flying mode with Ctrl+/
+                        (Some(VirtualKeyCode::Slash), ElementState::Pressed) => {
+                            if curr_modifier_state.ctrl() {
+                                game.state.world_state.is_flying =
+                                    !game.state.world_state.is_flying;
+                            }
+                        }
                         (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
                             window.set_cursor_visible(true);
                             window
