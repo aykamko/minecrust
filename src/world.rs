3d90644102c1380d7dc4285ae643634c1f678a28
Aleks <aykamko@gmail.com>
2023-12-27 17:33:20 -0500

    lower down to just 8 chunks for mobile for now

--- src/world.rs
+++ src/world.rs
@@ -206,7 +206,7 @@ cfg_if::cfg_if! {
 // The largest the world can be in xz dimension
 pub const MAX_CHUNK_WORLD_WIDTH: usize = 1024;
 // How many chunks are visible in xz dimension
-pub const VISIBLE_CHUNK_WIDTH: usize = 12;
+pub const VISIBLE_CHUNK_WIDTH: usize = 8;
 // Estimate of farthest z distance that is rendered
 pub const Z_FAR: f32 = 92.0;
     } else {
